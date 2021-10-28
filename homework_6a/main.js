2cdfef17ed2768b7bc457ebc0737b508d4557b20
jenjlee <jjlee3@andrew.cmu.edu>
2021-10-27 23:58:50 -0400

    hw 6a update

--- homework_6a/main.js
+++ homework_6a/main.js
@@ -2,20 +2,22 @@
 var glazeSelect = document.querySelectorAll(".glazing");
 var quantitySelect = document.querySelectorAll(".item_quantity");
 
-// changes background of selected glaze
+// changes button background of selected glaze
 function glazeActive() {
     glazeSelect.forEach(glazing => glazing.classList.remove("active"));
     this.classList.add("active");
 }
 
-// changes background of selected quantity
+// changes button background of selected quantity
 function quantityActive() {
     quantitySelect.forEach(item_quantity => item_quantity.classList.remove("active"));
     this.classList.add("active");
 }
 
+// click to activate change in button background 
 glazeSelect.forEach(glazing => glazing.addEventListener("click", glazeActive));
 quantitySelect.forEach(item_quantity => item_quantity.addEventListener("click", quantityActive));
+
 
 var order = function(bun, glazing, item_quantity, price) {
     const glazeChosen = document.querySelector(".glazing.active")
@@ -29,21 +31,26 @@ var order = function(bun, glazing, item_quantity, price) {
         this.price = quantityChosen.dataset.price;
     }
     
+// initialize cart    
 let cart = [];
 
+// add new, personalized orders to cart
 function addOrder(bun, glazing, item_quantity, price) {
     var item = new order(bun, glazing, item_quantity, price);
     cart.push(item);
 }    
 
-let overallCart = document.querySelectorAll('.add_to_cart_button');
 
+let overallCart = document.querySelectorAll('.add_to_cart');
+
+// loop to see how many orders are in the cart
 for(let i=0; i < overallCart.length; i++) {
     overallCart[i].addEventListener("click", ()=> {
         cartNum();
     })
 }
-    
+
+// checks to see how many items are in the cart
 function cartNum() {
     let itemNum = localStorage.getItem("cartNum");
     itemNum = parseInt(itemNum);
@@ -58,14 +65,13 @@ function cartNum() {
     }
 }
 
-let cartButton = document.querySelector('.add_to_cart_button');
+// intialize cart button
+let cartButton = document.querySelector('.add_to_cart');
 
+// when cart button is clicked, it calls the addOrder function
 if(cartButton) {
     cartButton.addEventListener('click',() => {
         addOrder();
-        alert("product added!");
-        //totalCost();
     })
 }
-
 }
